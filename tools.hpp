4946fc97b61b4f117d8015543ec80805ac0201ac
lucas8 <luc.linux@mailoo.org>
2012-06-09 11:03:00 +0200

    Ajout de 3 outils sur les points.
    Calcul du milieu, calcul du min et calcul du max.

--- tools.hpp
+++ tools.hpp
@@ -4,6 +4,7 @@
 #include <SDL/SDL.h>
 #include <iostream>
 #include <sstream>
+#include <algorithm>
 
 #include "point_dec.hpp"
 #include "vector2f.hpp"
@@ -11,11 +12,29 @@
 namespace sdl
 {
 	SDL_Rect makeRect(int x, int y, int w=0, int h=0);
-};
 
-template<typename T>
-std::ostream& operator<<(std::ostream& oss, const sdl::Point<T>& p);
-std::ostream& operator<<(std::ostream& oss, const sdl::Vector2f& v);
-std::ostream& operator<<(std::ostream& oss, const SDL_Rect& r);
+	template<typename T>
+		Point<T> middle(const Point<T> p1, const Point<T> p2)
+		{
+			T x = p1.x + p2.x;
+			x /= 2;
+			T y = p1.y + p2.y;
+			y /= 2;
+
+			return Point<T>(x, y);
+		}
+
+	template<typename T>
+		Point<T> min(const Point<T> p1, const Point<T> p2)
+		{
+			return Point<T>(std::min(p1.x, p2.x), std::min(p1.y, p2.y));
+		}
+
+	template<typename T>
+		Point<T> max(const Point<T> p1, const Point<T> p2)
+		{
+			return Point<T>(std::max(p1.x, p2.x), std::max(p1.y, p2.y));
+		}
+};
 
 #endif//DEF_SDLP_TOOLS
