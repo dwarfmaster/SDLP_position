be1e51eaf57dab7f45bf6dcab7e9771bf5c5f415
lucas8 <luc.linux@mailoo.org>
2012-06-09 20:16:26 +0200

    Mise des nouveaux fichiers dans l'header principal.

--- SDLP_position.hpp
+++ SDLP_position.hpp
@@ -4,6 +4,6 @@
 #include <SDLP_position/vector2f.hpp>
 #include <SDLP_position/point_dec.hpp>
 #include <SDLP_position/tools.hpp>
-#include <SDLP_position/accumulator.hpp>
+#include <SDLP_position/aabb.hpp>
 
 #endif//DEF_SDLP_POSITION_HPP
