0e60ea9fc2ba09705a49e601c08dec0cc7642d3f
lucas8 <luc.linux@mailoo.org>
2012-06-10 19:13:12 +0200

    Ajout de la fonction length entre 2 points.

--- tools.hpp
+++ tools.hpp
@@ -14,7 +14,7 @@ namespace sdl
 	SDL_Rect makeRect(int x, int y, int w=0, int h=0);
 
 	template<typename T>
-		Point<T> middle(const Point<T> p1, const Point<T> p2)
+		Point<T> middle(const Point<T>& p1, const Point<T>& p2)
 		{
 			T x = p1.x + p2.x;
 			x /= 2;
@@ -25,13 +25,22 @@ namespace sdl
 		}
 
 	template<typename T>
-		Point<T> min(const Point<T> p1, const Point<T> p2)
+		unsigned int length(const Point<T>& p1, const Point<T>& p2)
+		{
+			unsigned int xcar = (p1.x-p2.x) * (p1.x-p2.x);
+			unsigned int ycar = (p1.y-p2.y) * (p1.y-p2.y);
+
+			return static_cast<unsigned int>(std::sqrt(xcar + ycar));
+		}
+
+	template<typename T>
+		Point<T> min(const Point<T>& p1, const Point<T>& p2)
 		{
 			return Point<T>(std::min(p1.x, p2.x), std::min(p1.y, p2.y));
 		}
 
 	template<typename T>
-		Point<T> max(const Point<T> p1, const Point<T> p2)
+		Point<T> max(const Point<T>& p1, const Point<T>& p2)
 		{
 			return Point<T>(std::max(p1.x, p2.x), std::max(p1.y, p2.y));
 		}
